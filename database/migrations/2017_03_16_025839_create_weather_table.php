24ba083fdb4d336a373cfa6fa8ce99d609ff5219
Kanin Manoch <dietyboy@gmail.com>
2017-03-17 05:29:30 +0700

    init DB 3

--- database/migrations/2017_03_16_025839_create_weather_table.php
+++ database/migrations/2017_03_16_025839_create_weather_table.php
@@ -21,8 +21,8 @@ class CreateWeatherTable extends Migration
             $table->float('pressure');
             $table->string('relative_humidity');
             $table->string('soil_humidity');
-            $table->longText('pi_image');
-            $table->longText('matlab_image');
+            //$table->longText('pi_image');
+            //$table->longText('matlab_image');
             $table->timestamps();
         });
 
