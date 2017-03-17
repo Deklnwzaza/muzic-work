64d45774b31fdb01e30a8e30511a4215a4fd3074
Kanin Manoch <dietyboy@gmail.com>
2017-03-17 12:09:47 +0700

    fix bot

--- database/migrations/2017_03_16_025839_create_weather_table.php
+++ database/migrations/2017_03_16_025839_create_weather_table.php
@@ -21,8 +21,9 @@ class CreateWeatherTable extends Migration
             $table->float('pressure');
             $table->string('relative_humidity');
             $table->string('soil_humidity');
-            $table->longText('pi_image');
+            $table->bytea('pi_image');
             //$table->longText('matlab_image');
+
             $table->timestamps();
         });
 
